fba807b75dbcfe641f5692171f0932bb095ed812
TruongSinh Tran-Nguyen <i@truongsinh.pro>
2016-07-10 20:02:06 +0300

    Initial Commit

---
+++ bnwie/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  bnwie
+//
+//  Created by TruongSinh Tran-Nguyen on 7/10/16.
+//  Copyright © 2016 TruongSinh Tran-Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ bnwie/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  bnwie
+//
+//  Created by TruongSinh Tran-Nguyen on 7/10/16.
+//  Copyright © 2016 TruongSinh Tran-Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
